f128ec30689c2fb86ab05bf1ab0d93dbabbd612e
Łukasz K <lkipka@gmail.com>
2021-05-18 17:25:03 +0200

    Add project files.

---
+++ BeerCup/BeerCup/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BeerCup.ViewModels;
+using BeerCup.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BeerCup
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ BeerCup/BeerCup/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BeerCup.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BeerCup.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
