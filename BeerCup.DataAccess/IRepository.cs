38ebee39854f05e645a3488ac6dc0ddeb4250df8
Łukasz K <lkipka@gmail.com>
2021-05-21 11:34:14 +0200

    Added IRepository interface and Repository class

---
+++ BeerCup.DataAccess/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerCup.DataAccess
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        public IEnumerable<T> GetAll();
+        public T GetById(int id);
+        public void Insert(T entity);
+        public void Update(T entity);
+        public void Delete(int id);
+    }
+}
---
+++ BeerCup.DataAccess/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeerCup.DataAccess
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        private readonly BeerCupStorageContext _context;
+        private DbSet<T> entities; 
+
+        public Repository(BeerCupStorageContext context)
+        {
+            this._context = context;
+            entities = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return entities.SingleOrDefault(s => s.Id == id);
+        }
+
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            entities.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            entities.Update(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            T entity = entities.SingleOrDefault(s => s.Id == id);
+            entities.Add(entity);
+            _context.SaveChanges();
+        }
+    }
+}
