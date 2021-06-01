3c5f34d1cc555bf70fa1ad16bd48b94555d0d252
Łukasz K <lkipka@gmail.com>
2021-06-01 09:38:13 +0200

    BattlesController logic moved to base class (ApiControllerBase).

--- BeerCup.WebAPI/Controllers/BattlesController.cs
+++ BeerCup.WebAPI/Controllers/BattlesController.cs
@@ -14,7 +14,6 @@ namespace BeerCup.WebAPI.Controllers
     [Route("[controller]")]
     public class BattlesController : ApiControllerBase
     {
-
         public BattlesController(IMediator mediator) : base(mediator)
         {
         }
@@ -24,34 +23,18 @@ namespace BeerCup.WebAPI.Controllers
         public Task<IActionResult> GetAllBattles([FromQuery] GetBattlesRequest request)
         {
             return this.HandleRequest<GetBattlesRequest, GetBattlesResponse>(request);
-            
-            //var response = await this.mediator.Send(request);
-            //return Ok(response);
         }
 
         [HttpGet]
         [Route("{battleId}")]
-        public async Task<IActionResult> GetBattleById([FromRoute] int battleId)
+        public Task<IActionResult> GetBattleById([FromRoute] int battleId)
         {
             var request = new GetBattleByIdRequest()
             {
                 BattleId = battleId
             };
 
-            var response = await this.mediator.Send(request);
-            return Ok(response);
+            return this.HandleRequest<GetBattleByIdRequest, GetBattleByIdResponse>(request);
         }
-
-        //public IEnumerable<Battle> GetAllBattles() => this.battleRepository.GetAll();
-
-        //[HttpGet]
-        //[Route("battleId")]
-        //public async Task<IActionResult> GetBattleById(int id)
-        //{
-        //    var response = await this.mediator.Send(id);
-        //    return Ok(response);
-        //}
-
-        //public Battle GetBattleById(int battleId) => this.battleRepository.GetById(battleId);
     }
 }
