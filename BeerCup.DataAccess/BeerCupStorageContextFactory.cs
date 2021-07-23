cfed894dc6b11bedf1d2f6eaea72b63c714a92fc
Lukasz Kipka <lukasz.kipka@pb.com>
2021-07-23 09:32:11 +0200

    Replaced db connStr from local to Azure

--- BeerCup.DataAccess/BeerCupStorageContextFactory.cs
+++ BeerCup.DataAccess/BeerCupStorageContextFactory.cs
@@ -14,7 +14,10 @@ namespace BeerCup.DataAccess
         {
             var optionsBuilder = new DbContextOptionsBuilder<BeerCupStorageContext>();
             //todo: zmień logowanie na autentykację windowsową
-            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=BeerCupStorage;Persist Security Info=True;User ID=sa;Password=sysadmin1.");
+            //localDb
+            //optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=BeerCupStorage;Persist Security Info=True;User ID=sa;Password=sysadmin1.");
+            //Azure
+            optionsBuilder.UseSqlServer("Server=tcp:beercup-db.database.windows.net,1433;Initial Catalog=BeerCupStorage;Persist Security Info=False;User ID=lukasz;Password=ONxPFt4AMKH0WfXa;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             return new BeerCupStorageContext(optionsBuilder.Options);
         }
     }
