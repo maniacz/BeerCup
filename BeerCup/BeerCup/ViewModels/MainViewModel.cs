4bb87bb400cc1281469ddc3d03883d5016dae3b0
Lukasz Kipka <lukasz.kipka@pb.com>
2021-08-19 13:20:34 +0200

    Added empty MainView and MainViewModel

---
+++ BeerCup/BeerCup/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BeerCup.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerCup.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+    }
+}
