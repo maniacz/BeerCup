d7bf6a03582ee7a8659871e66440be0fdfd8d354
Lukasz Kipka <lukasz.kipka@pb.com>
2021-11-02 15:59:22 +0100

    Common mocks moved to Initialize method in class constructor

--- MobileApp.Tests/BattleDataServiceTests.cs
+++ MobileApp.Tests/BattleDataServiceTests.cs
@@ -10,17 +10,29 @@ using Xunit;
 
 namespace MobileApp.Tests
 {
-    public class BattleDataServiceTests
+    public class BattleDataServiceTests : IDisposable
     {
+        private Mock<IGenericRepository> _mockGenericRepository;
+        private Mock<ISettingsService> _mockSettingsService;
+
+        public BattleDataServiceTests()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _mockGenericRepository = new Mock<IGenericRepository>();
+            _mockSettingsService = new Mock<ISettingsService>();
+        }
+
         [Fact]
         public async Task GetCurrentRunningBattle_ReturnsBattle()
         {
             //Arrange
-            var mockGenericRepository = new Mock<IGenericRepository>();
-            var mockSettingsService = new Mock<ISettingsService>();
-            mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<Battle>>(It.IsAny<string>(), It.IsAny<string>()))
+            _mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<Battle>>(It.IsAny<string>(), It.IsAny<string>()))
                 .ReturnsAsync(() => new ApiResponse<Battle> { Data = new Battle() });
-            var sut = new BattleDataService(mockGenericRepository.Object, mockSettingsService.Object);
+            var sut = new BattleDataService(_mockGenericRepository.Object, _mockSettingsService.Object);
 
             //Act
             var currentRunningBattle = await sut.GetCurrentRunningBattle();
@@ -33,9 +45,7 @@ namespace MobileApp.Tests
         public async Task GetBattleResults_ReturnsFiveResults()
         {
             //Arrange
-            var mockGenericRepository = new Mock<IGenericRepository>();
-            var mockSettingsService = new Mock<ISettingsService>();
-            mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<List<Result>>>(It.IsAny<string>(), It.IsAny<string>()))
+            _mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<List<Result>>>(It.IsAny<string>(), It.IsAny<string>()))
                 .ReturnsAsync(() => new ApiResponse<List<Result>> { Data = new List<Result> {
                     new Result(1, "BroGar", 30, 60.0M),
                     new Result(2, "Venom", 10, 20.0M),
@@ -43,13 +53,17 @@ namespace MobileApp.Tests
                     new Result(4, "Hołda", 3, 6.0M),
                     new Result(5, "Bastion", 2, 4.0M)
                 } });
-            var sut = new BattleDataService(mockGenericRepository.Object, mockSettingsService.Object);
+            var sut = new BattleDataService(_mockGenericRepository.Object, _mockSettingsService.Object);
 
             //Act
             var results = await sut.GetBattleResults(1);
 
             //Assert
             Assert.Equal(5, results.Count);
+        }
+
+        public void Dispose()
+        {
         }
     }
 }
