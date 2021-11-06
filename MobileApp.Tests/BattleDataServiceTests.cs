25a011f0c7b7a87f49a98a227d629fb77bbdd1e8
Lukasz Kipka <lukasz.kipka@pb.com>
2021-11-06 10:38:39 +0100

    More tests added

--- MobileApp.Tests/BattleDataServiceTests.cs
+++ MobileApp.Tests/BattleDataServiceTests.cs
@@ -14,6 +14,7 @@ namespace MobileApp.Tests
     {
         private Mock<IGenericRepository> _mockGenericRepository;
         private Mock<ISettingsService> _mockSettingsService;
+        private BattleDataService _sut;
 
         public BattleDataServiceTests()
         {
@@ -24,6 +25,7 @@ namespace MobileApp.Tests
         {
             _mockGenericRepository = new Mock<IGenericRepository>();
             _mockSettingsService = new Mock<ISettingsService>();
+            _sut = new BattleDataService(_mockGenericRepository.Object, _mockSettingsService.Object);
         }
 
         [Fact]
@@ -32,10 +34,9 @@ namespace MobileApp.Tests
             //Arrange
             _mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<Battle>>(It.IsAny<string>(), It.IsAny<string>()))
                 .ReturnsAsync(() => new ApiResponse<Battle> { Data = new Battle() });
-            var sut = new BattleDataService(_mockGenericRepository.Object, _mockSettingsService.Object);
 
             //Act
-            var currentRunningBattle = await sut.GetCurrentRunningBattle();
+            var currentRunningBattle = await _sut.GetCurrentRunningBattle();
 
             //Assert
             Assert.IsType<Battle>(currentRunningBattle);
@@ -53,14 +54,71 @@ namespace MobileApp.Tests
                     new Result(4, "Hołda", 3, 6.0M),
                     new Result(5, "Bastion", 2, 4.0M)
                 } });
-            var sut = new BattleDataService(_mockGenericRepository.Object, _mockSettingsService.Object);
 
             //Act
-            var results = await sut.GetBattleResults(1);
+            var results = await _sut.GetBattleResults(1);
 
             //Assert
             Assert.Equal(5, results.Count);
         }
+
+        [Fact]
+        public async Task GetTodaysBattle_ReturnsBattleWhenBattleIsScheduledForToday()
+        {
+            //Arrange
+            _mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<Battle>>(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(() => new ApiResponse<Battle> { Data = new Battle() });
+
+            //Act
+            var response = await _sut.GetTodaysBattle();
+
+            //Assert
+            Assert.IsType<Battle>(response);
+        }
+
+        [Fact]
+        public async Task GetFinishedBattles_ReturnsListOfBattles()
+        {
+            //Arrange
+            _mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<List<Battle>>>(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(() => new ApiResponse<List<Battle>> { Data = new List<Battle>() });
+
+            //Act
+            var response = await _sut.GetFinishedBattles();
+
+            //Assert
+            Assert.IsType<List<Battle>>(response);
+        }
+
+        [Fact]
+        public async Task GetBattleUserVotes_ReturnsListOfVote()
+        {
+            //Arrange
+            var battleId = 1;
+            _mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<List<Vote>>>(It.Is<string>(s => s.Contains(battleId.ToString())), It.IsAny<string>()))
+                .ReturnsAsync(() => new ApiResponse<List<Vote>> { Data = new List<Vote>() });
+
+            //Act
+            var response = await _sut.GetBattleUserVotes(battleId);
+
+            //Assert
+            Assert.IsType<List<Vote>>(response);
+        }
+
+        [Fact]
+        public async Task GetAllBattles_ReturnsListOfBattles()
+        {
+            //Arrange
+            _mockGenericRepository.Setup(x => x.GetAsync<ApiResponse<List<Battle>>>(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(() => new ApiResponse<List<Battle>> { Data = new List<Battle>() });
+
+            //Act
+            var response = await _sut.GetAllBattles();
+
+            //Assert
+            Assert.IsType<List<Battle>>(response);
+        }
+
 
         public void Dispose()
         {
